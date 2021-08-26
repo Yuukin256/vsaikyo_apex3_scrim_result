c94f5d41e011eccb15579209fb25d4f251c127a1
Yuukin256 <Yuukin256@gmail.com>
2021-08-26 17:48:17 +0900

    404.js をtsx化

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/layouts/layout';
+import Seo from '../components/layouts/seo';
+
+const NotFoundPage: React.VFC = () => (
+  <Layout>
+    <Seo title="404 Not found" />
+    <h1>404 Not Found</h1>
+    <p>おっと、存在しないページにアクセスしたようです。</p>
+  </Layout>
+);
+
+export default NotFoundPage;
